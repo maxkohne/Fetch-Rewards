5ca4c1fdde4a1e2817f09c951bcbd5b7d2f2bd10
Max Kohne <kohne.max@gmail.com>
2025-03-18 17:51:24 -0700

    core:util module

---
+++ core/util/build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    alias(libs.plugins.fetchrewards.android.library)
+}
+
+android {
+    namespace = "com.maxkohne.fetchrewards.core.util"
+}
---
+++ core/util/src/main/java/com/maxkohne/fetchrewards/core/util/DispatcherModule.kt
@@ -0,0 +1,37 @@
+package com.maxkohne.fetchrewards.core.util
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class DefaultDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class IoDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class MainDispatcher
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object DispatcherModule {
+    @DefaultDispatcher
+    @Provides
+    fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @IoDispatcher
+    @Provides
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @MainDispatcher
+    @Provides
+    fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+}
